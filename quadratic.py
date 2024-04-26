fdead97d9bc701f34958185a7fd344d21dbdd4ca
mateoirustaa <162723360+mateoirustaa@users.noreply.github.com>
2024-04-26 00:53:54 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,10 +1,11 @@
 # Replace the "ANSWER HERE" for your answer
+import math
 
 def roots(a, b, c):
     det = b**2 - 4*a*c
 
     if det > 0:
-        return f'({(-b + math.sqrt(det))/2*a}, {(-b - math.sqrt(det))/2*a})'
+        return f'{(-b + math.sqrt(det)) / (2*a), (-b - math.sqrt(det)) / (2*a)}
     elif det == 0:
         return f'({-b/2})'
     elif det < 0:
@@ -33,8 +34,8 @@ def to_string(a, b, c):
 
 def derivation(a, b):
     if a != 0 and b != 0:
-        return f'f'(x) = {a*2} * x + {b}'
+        return f"f'(x) = {a*2} * x + {b}"
     elif a == 0 and b != 0:
-        return f'f'(x) = {b}'
+        return f"f'(x) = {b}"
     elif a != 0 and b == 0:
-        return f'f'(x) = 0' 
+        return f"f'(x) = 0" 
